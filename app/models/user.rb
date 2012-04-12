2a63c52fa39ff07c43476b76c202a62cfc73df00
Priyanka <pgurnani01@gmail.com>
2012-04-12 23:52:31 +0530

    make changes

---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+   attr_accessible :password, :username
+  validates_length_of :password, :minimum => 3, :message => "password must be at least 3 characters long", :if => :password
+  validates_confirmation_of :password, :message => "should match confirmation", :if => :password
+end
